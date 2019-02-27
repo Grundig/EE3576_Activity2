dae415f439148b9ad2e13df20ba5736572c969a7
Kim Beattie <kimbeattie14@gmail.com>
2019-02-27 00:17:28 +0000

    Timing change

--- sketches/Act2_2_test/Act2_2_test.ino
+++ sketches/Act2_2_test/Act2_2_test.ino
@@ -14,15 +14,15 @@ void setup() {
   int mid_speed_pin = 12;
   int high_speed_pin = 13;
   
-  int adjust_speed_time=250;
-  int target_speed_time=1000;
+  int adjust_speed_time=200;
+  int target_speed_time=200;
   int buttons_check_time=200;
   
   our_sys.setup_motor_pushbuttons(start_pin, stop_pin, reverse_pin);
   our_sys.setup_motor(motor_pin, direction_pin);
   our_sys.setup_speed_measure(speed_pin);
   our_sys.set_time_intervals(buttons_check_time, adjust_speed_time, target_speed_time);
-  our_sys.setup_speed_pushbuttons(low_speed_pin, mid_speed_pin, high_speed_pin);
+//  our_sys.setup_speed_pushbuttons(low_speed_pin, mid_speed_pin, high_speed_pin);
 
   Serial.begin(9600);
 }
@@ -31,5 +31,4 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   our_sys.system_execute();
-  delay(1000);
 }
