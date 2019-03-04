a62f14d102b97d2bc746a8b050eec1563c5c28b0
Kim Beattie <kimbeattie14@gmail.com>
2019-03-04 11:02:17 +0000

    Part 4

---
+++ libraries/Act2_4/Act2_4.h
@@ -0,0 +1,266 @@
+#ifndef Act2_4_h
+#define Act2_4_h
+
+#include<PushButton.h>
+#include<SerialPrinterPlotter.h>
+#include<basic_speed_PID.h>
+#include<DCmotor.h>
+#include<IntervalCheckTimer.h>
+#include<InterruptBasedSpeedMeasure.h>
+
+#include<stdio.h>
+
+command_list_enum command;
+double target_speed = 2000;
+double pid_out;
+static unsigned int store[100];
+int pr_counter=0;
+int i=0;
+
+class Act2_4{
+	
+	protected:
+
+		// Objects
+		SerialPrinterPlotter plotter;
+		basic_speed_PID pid;
+		inputs pushbuttons;
+		HBridgeDCmotor HBmotor;
+		InterruptSpeedMeasure rotate_count;
+		IntervalCheckTimer button_time_check, target_speed_check, plot_time_check;
+
+		// Values
+		double motor_low_speed = 77;  // 30% rated speed (6000 rpm)
+		double motor_mid_speed = 128;  // 50% rated speed
+		double motor_high_speed = 204;  // 80% rated speed
+		
+		double motor_low_speed_rpm = 1800;  // 30% rated speed (6000 rpm)
+		double motor_mid_speed_rpm = 3000;  // 50% rated speed
+		double motor_high_speed_rpm = 4800;  // 80% rated speed
+		
+		double pctg_error = 50; // Percentage error 
+		
+		// Global variabled
+  		int target_speed_time;
+ 		int buttons_check_time;
+ 		
+		
+	public:
+		
+		// Set up direction push buttons
+		void setup_motor_pushbuttons(int start_pin, int stop_pin, int reverse_pin)
+		{
+			// Set long push
+			unsigned long int mininterval_ms=2000;
+			
+			// Labelling and assigning each pin
+  			in_push_button start_but(start_pin, start, mininterval_ms);
+  			in_push_button stop_but(stop_pin, stop, mininterval_ms);
+  			in_push_button reverse_but(reverse_pin, reverse, mininterval_ms);
+  			
+  			// Add push button into system
+  			pushbuttons.add_in_push_button(start_but);
+ 			pushbuttons.add_in_push_button(stop_but);
+  			pushbuttons.add_in_push_button(reverse_but);
+  			
+		}
+		
+		// Set up speed push buttons
+		void setup_speed_pushbuttons(int low_speed_pin, int mid_speed_pin, int high_speed_pin)
+		{
+			// Set long push
+			unsigned long int mininterval_ms=2000;
+			
+			// Labelling and assigning each pin
+  			in_push_button low_but(low_speed_pin, low, mininterval_ms);
+  			in_push_button mid_but(mid_speed_pin, mid, mininterval_ms);
+  			in_push_button high_but(high_speed_pin, high, mininterval_ms);
+  			
+  			// Add push button into system
+  			pushbuttons.add_in_push_button(low_but);
+ 			pushbuttons.add_in_push_button(mid_but);
+  			pushbuttons.add_in_push_button(high_but);
+
+		}
+		
+		// Set up motor
+		void setup_motor(int pwm_motor_pin, int direction_pin)
+		{
+			HBmotor.setup_HBridgeDCmotor(pwm_motor_pin, direction_pin);
+		}
+		
+		// Set up speed measuring unit, hall effect sensor
+		void setup_speed_measure(ArduinoInterruptNames speed_pin)  // int_0, pin 2
+		{
+			rotate_count.setupSpeedMeasure(speed_pin);
+		}
+		
+		// Set times between checks for components and system
+		void set_time_intervals(int buttons_check_time, int target_speed_time)
+		{
+			button_time_check.setInterCheck(buttons_check_time);
+			target_speed_check.setInterCheck(target_speed_time);
+			plot_time_check.setInterCheck(200);
+		}
+		        
+		// Determine motor speed command (low, mid, high) from pushbuttons
+		void motor_speed_input(command_list_enum command)
+		{	
+			switch (command)
+			{
+				case start:
+        		Serial.println(" Start button pressed");
+        		HBmotor.start();
+        		break;
+        		
+        		case stop:
+        		Serial.println("    Stop button pressed");  
+        		HBmotor.stop();
+        		break;
+        		
+        		case reverse:
+        		Serial.println("        Reverse button pressed");
+        		HBmotor.changedir();
+        		break;
+        
+        		case low:
+        		Serial.println(" Low button pressed");
+        		HBmotor.setSpeedPWM(motor_low_speed);
+        		target_speed = motor_low_speed_rpm;
+        		break;
+        		
+        		case mid:
+        		Serial.println("    Mid button pressed");  
+        		HBmotor.setSpeedPWM(motor_mid_speed);
+        		target_speed = motor_mid_speed_rpm;
+				break;
+        		
+        		case high:
+        		Serial.println("        High button pressed");
+        		HBmotor.setSpeedPWM(motor_high_speed);
+        		target_speed = motor_high_speed_rpm;
+        		break;
+        		
+        		default:
+          		break;
+			}
+			
+		}
+		
+		// Get motor speed from Hall effect sensor (RPM)
+		double read_motor_speed()
+		{
+			double RPM=rotate_count.getRPMandUpdate();
+    		return RPM;
+		}
+		
+		// Execute the system task
+		void system_execute()
+		{		
+			bool success_command;
+			double curr_speed;
+			
+			// Check buttons
+			if (button_time_check.isMinChekTimeElapsedAndUpdate())
+			{
+				
+				// Get push button command
+				success_command = pushbuttons.check_n_get_command(command);
+			
+				if (success_command)
+				{
+					motor_speed_input(command);
+				}
+				
+			}
+				
+				
+			// PID controller to adjust speed to set point, use target speed check
+			if(target_speed_check.isMinChekTimeElapsedAndUpdate())
+			{
+				curr_speed = read_motor_speed();
+				if(HBmotor.isStarted())
+				{
+					pid_out = pid.ComputePID_output(target_speed, curr_speed);
+					HBmotor.setSpeedPWM(pid_out);
+				}
+				else
+					pid.reset_pidcontrol();
+				
+				plot_and_replay(pctg_error, curr_speed);
+			}
+			
+			// Plot in serial plotter
+//			if(plot_time_check.isMinChekTimeElapsedAndUpdate())
+//				{
+//					Serial.print(0);  // To freeze the lower limit
+//					Serial.print(" ");
+//					Serial.print(6000);  // To freeze the upper limit
+//					Serial.print(" ");
+//					Serial.print(target_speed);
+//					Serial.print(" ");
+//					Serial.println(curr_speed);      // X axis is number of samples, i.e, sampled every 0.2 ms, so 100 samples taken in 20 s
+//				}
+		}	
+		
+		void plot_and_replay(double pctg_error, double curr_speed)
+		{
+			int settled_count = 20;
+	
+			// Error bars
+			double min_error = target_speed * (1-(pctg_error/100));
+			double max_error = target_speed * (1+(pctg_error/100));
+
+			if ((pr_counter < settled_count) && (HBmotor.isStarted()))
+			{
+				// Store current speed into array value and print 
+				store[i] = curr_speed;
+				
+				// Print array in serial monitor
+				Serial.print(i);
+				Serial.print("   ");
+				Serial.println(curr_speed);
+				
+				i++;
+			
+				// If current speed is within error bars
+				if ((curr_speed <= max_error) && (curr_speed >= min_error))
+				{
+					pr_counter++;
+				}
+			
+				// Otherwise, reset counter
+				else
+				{
+					pr_counter = 0;
+					//Serial.println("COUNTER RESET");
+				}
+			}
+			
+				
+			// Replay data
+			if (pr_counter == settled_count)
+			{
+				Serial.println("Speed settled - recording ended");
+				Serial.println("Replay data:");
+				Serial.print("Sample   ");
+				Serial.println("RPM");
+				
+				int j=0;
+
+				for(j=0;j<i;j++)
+				{
+					Serial.print(j);
+					Serial.print("        ");
+					Serial.println(store[j]);
+				}
+					
+				pr_counter ++;
+				
+			}
+		}
+			
+};
+
+
+#endif
